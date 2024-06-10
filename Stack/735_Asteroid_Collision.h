b2906796959a147367060991d8fe7ee17d55f6e6
ordinarydennis <ordinarydennis@gmail.com>
2024-06-11 07:31:04 +0900

    LeetCode75  Stack - 735. Asteroid Collision(retry) https://leetcode.com/problems/asteroid-collision/editorial/?envType=study-plan-v2&envId=leetcode-75

--- Stack/735_Asteroid_Collision.h
+++ Stack/735_Asteroid_Collision.h
@@ -4,52 +4,35 @@ public:
 
 		std::stack<int> st;
 
-		int sign = 0;
-
 		for (int i = 0; i < asteroids.size(); i++)
 		{
-			if (st.empty())
-			{
-				st.push(asteroids[i]);
-				sign = 0 < asteroids[i] ? 1 : -1;
-			}
+			bool isExplode = false;
 
-			int targetSing = 0 < asteroids[i] ? 1 : -1;
-
-			if (sign == targetSing)
+			while (st.size() && 0 < st.top() && 0 > asteroids[i])
 			{
-				st.push(asteroids[i]);
-			}
-			else
-			{
-				if (1 == targetSing || -1 == sign)
+				if (abs(st.top()) < abs(asteroids[i]))
 				{
-					st.push(asteroids[i]);
-					sign = targetSing;
-				}
-				else
-				{
-					while (st.size())
-					{
-						auto t = st.top();
-
-						sign = 0 < st.top() ? 1 : -1;
-
-						if (abs(t) <= abs(asteroids[i]))
-						{
-							st.pop();
-						}
-						else
-						{
-							break;
-						}
-					}
+					st.pop();
+					continue;
 				}
 
+				if (abs(st.top()) == abs(asteroids[i]))
+				{
+					st.pop();
+				}
+
+				isExplode = true;
+				break;
+			}
+
+			if (false == isExplode)
+			{
+				st.push(asteroids[i]);
 			}
 		}
 
 		vector<int> ret;
+		ret.reserve(st.size());
 
 		while (st.size())
 		{
@@ -60,5 +43,49 @@ public:
 		std::reverse(ret.begin(), ret.end());
 
 		return ret;
+
+	}
+};
+
+//https://leetcode.com/problems/asteroid-collision/editorial/?envType=study-plan-v2&envId=leetcode-75
+class Solution {
+public:
+	vector<int> asteroidCollision(vector<int>& asteroids) {
+		stack<int> st;
+
+		for (int asteroid : asteroids) {
+			int flag = 1;
+			while (!st.empty() && (st.top() > 0 && asteroid < 0)) {
+				// If the top asteroid in the stack is smaller, then it will explode.
+				// Hence pop it from the stack, also continue with the next asteroid in the stack.
+				if (abs(st.top()) < abs(asteroid)) {
+					st.pop();
+					continue;
+				}
+				// If both asteroids are the same size, then both asteroids will explode.
+				// Pop the asteroid from the stack; also, we won't push the current asteroid to the stack.
+				else if (abs(st.top()) == abs(asteroid)) {
+					st.pop();
+				}
+
+				// If we reach here, the current asteroid will be destroyed
+				// Hence, we should not add it to the stack
+				flag = 0;
+				break;
+			}
+
+			if (flag) {
+				st.push(asteroid);
+			}
+		}
+
+		// Add the asteroids from the stack to the vector in the reverse order.
+		vector<int> remainingAsteroids(st.size());
+		for (int i = remainingAsteroids.size() - 1; i >= 0; i--) {
+			remainingAsteroids[i] = st.top();
+			st.pop();
+		}
+
+		return remainingAsteroids;
 	}
 };
