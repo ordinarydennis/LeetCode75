d6e924f6c14e0c0bf5ab97ff1e6752520a1874d9
ordinarydennis <ordinarydennis@gmail.com>
2024-06-12 14:24:54 +0900

    LeetCode75 LinkedList - 2095. Delete the Middle Node of a Linked List(10m)

---
+++ LinkedList/2095_Delete_the_Middle_Node_of_a_Linked_List.h
@@ -0,0 +1,64 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+
+	int GetListSize(ListNode* head)
+	{
+		ListNode* cur = head;
+
+		int count = 0;
+
+		while (cur)
+		{
+			cur = cur->next;
+			count++;
+		}
+
+		return count;
+	}
+
+public:
+	ListNode* deleteMiddle(ListNode* head) {
+
+		int count = GetListSize(head);
+
+		if (1 == count)
+		{
+			return nullptr;
+		}
+
+		ListNode* pre = nullptr;
+
+		ListNode* cur = head;
+
+		int mid = count / 2;
+
+		int i = 0;
+
+		while (cur)
+		{
+			if (i == mid)
+			{
+				pre->next = cur->next;
+				break;
+			}
+			else
+			{
+				pre = cur;
+				cur = cur->next;
+				i++;
+			}
+			
+		}
+		
+		return head;
+	}
+};
