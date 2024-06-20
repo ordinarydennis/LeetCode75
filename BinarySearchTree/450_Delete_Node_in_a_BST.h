c0e5dd70a430407b611810d009b4387f62de6c33
ordinarydennis <ordinarydennis@gmail.com>
2024-06-21 07:08:06 +0900

    LeetCode75 Binary Tree - DFS - 450. Delete Node in a BST(read editorial) https://leetcode.com/problems/delete-node-in-a-bst/editorial/?envType=study-plan-v2&envId=leetcode-75

--- BinarySearchTree/450_Delete_Node_in_a_BST.h
+++ BinarySearchTree/450_Delete_Node_in_a_BST.h
@@ -1,53 +1,53 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
-
-	TreeNode* dfs(TreeNode* root, int key)
-	{
-		if (!root)
-		{
-			return nullptr;
-		}
-
-		if (root->val == key)
-		{
-			auto* n = root->right;
-
-			while (n && n->left)
-				n = n->left;
-
-			if (n)
-			{
-				n->left = root->left;
-				return n;
-			}
-			else
-			{
-				return root->left;
-			}
-		}
-
-		if (root->val < key)
-			root->right = dfs(root->right, key);
-		else
-			root->left = dfs(root->left, key);
-
-		return root;
-	}
-
 public:
-	TreeNode* deleteNode(TreeNode* root, int key) {
+    /*
+    One step right and then always left
+    */
+    int successor(TreeNode* root) {
+        root = root->right;
+        while (root->left != nullptr) root = root->left;
+        return root->val;
+    }
 
-		return dfs(root, key);
+    /*
+    One step left and then always right
+    */
+    int predecessor(TreeNode* root) {
+        root = root->left;
+        while (root->right != nullptr) root = root->right;
+        return root->val;
+    }
 
-	}
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if (root == nullptr) return nullptr;
+
+        // delete from the right subtree
+        if (key > root->val) {
+            root->right = deleteNode(root->right, key);
+        }
+        // delete from the left subtree
+        else if (key < root->val) {
+            root->left = deleteNode(root->left, key);
+        }
+        // delete the current node
+        else {
+            // the node is a leaf
+            if (root->left == nullptr && root->right == nullptr) {
+                root = nullptr;
+            }
+            // the node is not a leaf and has a right child
+            else if (root->right != nullptr) {
+                root->val = successor(root);
+                root->right = deleteNode(root->right, root->val);
+            }
+            // the node is not a leaf, has no right child, and has a left child    
+            else {
+                root->val = predecessor(root);
+                root->left = deleteNode(root->left, root->val);
+            }
+        }
+        return root;
+    }
 };
+
+https://leetcode.com/problems/delete-node-in-a-bst/?envType=study-plan-v2&envId=leetcode-75
