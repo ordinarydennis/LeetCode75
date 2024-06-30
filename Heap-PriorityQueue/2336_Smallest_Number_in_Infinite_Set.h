114ce05e30b19d9d6a598d8e45b743dd453b6568
ordinarydennis <ordinarydennis@gmail.com>
2024-07-01 07:07:14 +0900

    LeetCode75 Heap / Priority Queue - 2336. Smallest Number in Infinite Set(read editorial) https://leetcode.com/problems/smallest-number-in-infinite-set/editorial/?envType=study-plan-v2&envId=leetcode-75

--- Heap-PriorityQueue/2336_Smallest_Number_in_Infinite_Set.h
+++ Heap-PriorityQueue/2336_Smallest_Number_in_Infinite_Set.h
@@ -46,3 +46,82 @@ private:
  * int param_1 = obj->popSmallest();
  * obj->addBack(num);
  */
+
+class SmallestInfiniteSet {
+public:
+	SmallestInfiniteSet() {
+	}
+
+	int popSmallest() {
+
+		if (list.size())
+		{
+			int n = *list.begin();
+			list.erase(n);
+			return n;
+		}
+
+		return nextPop++;
+
+	}
+
+	void addBack(int num) {
+
+		if (nextPop <= num)
+			return;
+
+		list.insert(num);
+	}
+
+private:
+	set<int> list;
+	int nextPop = 1;
+
+};
+
+
+class SmallestInfiniteSet {
+public:
+	SmallestInfiniteSet() {
+
+	}
+
+	int popSmallest() {
+
+		if (q.size())
+		{
+			int n = q.top();
+			q.pop();
+			s.erase(n);
+			return n;
+		}
+
+		return nextPop++;
+	}
+
+	void addBack(int num) {
+
+		if (s.count(num))
+			return;
+
+		if (nextPop <= num)
+			return;
+
+		q.push(num);
+		s.insert(num);
+	}
+
+
+private:
+	int nextPop = 1;
+	priority_queue<int, vector<int>, greater<int>> q;
+	unordered_set<int> s;
+};
+
+
+/**
+ * Your SmallestInfiniteSet object will be instantiated and called as such:
+ * SmallestInfiniteSet* obj = new SmallestInfiniteSet();
+ * int param_1 = obj->popSmallest();
+ * obj->addBack(num);
+ */
