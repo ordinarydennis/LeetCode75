24d60cf19a6da4be36f82a9e5c164096bc15b55b
ordinarydennis <ordinarydennis@gmail.com>
2024-07-08 06:59:07 +0900

    LeetCode75 Heap / Priority Queue - 162. Find Peak Element(read editorial) https://leetcode.com/problems/find-peak-element/submissions/1313326199/?envType=study-plan-v2&envId=leetcode-75

--- BinarySearch/162_Find_Peak_Element.h
+++ BinarySearch/162_Find_Peak_Element.h
@@ -1,71 +1,114 @@
 class Solution {
 public:
-    int findPeakElement(vector<int>& nums) {
+	int findPeakElement(vector<int>& nums) {
 
 
-        for (int i = 1; i < nums.size(); i++)
-        {
-            if (i < nums.size() - 1)
-            {
-                if (nums[i - 1] < nums[i] && nums[i] > nums[i + 1])
-                {
-                    return i;
-                }
-            }
-            else
-            {
-                if (nums[i - 1] < nums[i])
-                {
-                    return i;
-                }
-            }
-        }
+		for (int i = 1; i < nums.size(); i++)
+		{
+			if (i < nums.size() - 1)
+			{
+				if (nums[i - 1] < nums[i] && nums[i] > nums[i + 1])
+				{
+					return i;
+				}
+			}
+			else
+			{
+				if (nums[i - 1] < nums[i])
+				{
+					return i;
+				}
+			}
+		}
 
-        return 0;
-    }
+		return 0;
+	}
 };
 
 
 class Solution {
 
-    int dfs(int s, int e, vector<int>& nums)
-    {
-        if (s == e)
-        {
-            return 0;
-        }
+	int dfs(int s, int e, vector<int>& nums)
+	{
+		if (s == e)
+		{
+			return 0;
+		}
 
-        if (e - 1 == s)
-        {
-            return nums[s];
-        }
+		if (e - 1 == s)
+		{
+			return nums[s];
+		}
 
-        int m = (s + e) / 2;
+		int m = (s + e) / 2;
 
-        int l = dfs(s, m, nums);
-        int r = dfs(m + 1, e, nums);
+		int l = dfs(s, m, nums);
+		int r = dfs(m + 1, e, nums);
 
-        if (l < nums[m] && nums[m] > r)
-        {
-            ret = m;
-        }
+		if (l < nums[m] && nums[m] > r)
+		{
+			ret = m;
+		}
 
-        return nums[l] < nums[r] ? nums[r] : nums[l];
-    }
+		return nums[l] < nums[r] ? nums[r] : nums[l];
+	}
 
 
-    int ret = 0;
+	int ret = 0;
 
 public:
-    int findPeakElement(vector<int>& nums) {
+	int findPeakElement(vector<int>& nums) {
 
-        if (nums.size() == 2)
-        {
-            return nums[0] < nums[1] ? 1 : 0;
-        }
+		if (nums.size() == 2)
+		{
+			return nums[0] < nums[1] ? 1 : 0;
+		}
 
-        dfs(0, nums.size(), nums);
+		dfs(0, nums.size(), nums);
 
-        return ret;
-    }
+		return ret;
+	}
+};
+
+
+class Solution {
+
+	int dfs(int s, int e, vector<int>& nums)
+	{
+		if (s == e)
+			return s;
+
+		int m = (s + e) / 2;
+
+		if (nums[m] > nums[m + 1])
+			return dfs(s, m, nums);
+		else
+			return dfs(m + 1, e, nums);
+	}
+
+public:
+	int findPeakElement(vector<int>& nums) {
+
+		return dfs(0, nums.size() - 1, nums);
+
+	}
+};
+
+class Solution {
+public:
+	int findPeakElement(vector<int>& nums) {
+
+		int l = 0, r = nums.size() - 1;
+
+		while (l < r)
+		{
+			int mid = (l + r) / 2;
+
+			if (nums[mid] > nums[mid + 1])
+				r = mid;
+			else
+				l = mid + 1;
+		}
+		return l;
+	}
 };
