de33ec3ae73ef2cfde0a6ba34000dd17ea0328e1
ordinarydennis <ordinarydennis@gmail.com>
2024-07-09 12:57:15 +0900

    LeetCode75 Binary Search - 875. Koko Eating Bananas(read editorial) https://leetcode.com/problems/koko-eating-bananas/editorial/?envType=study-plan-v2&envId=leetcode-75

--- BinarySearch/875_Koko_Eating_Bananas.h
+++ BinarySearch/875_Koko_Eating_Bananas.h
@@ -4,3 +4,35 @@ public:
 
 	}
 };
+
+class Solution {
+public:
+	int minEatingSpeed(vector<int>& piles, int h) {
+
+		int speed = 1;
+
+		while (true)
+		{
+			long long n = 0;
+
+			for (auto p : piles)
+			{
+				n += p / speed;
+				n += (0 != p % speed) ? 1 : 0;
+
+				if (n > h)
+				{
+					break;
+				}
+			}
+
+			if (n <= h)
+				return speed;
+			else
+				speed++;
+		}
+
+		return 0;
+	}
+};
+
