c06ac632d561612d80037bdaa73db8a1ba8c4a07
ordinarydennis <ordinarydennis@gmail.com>
2024-07-29 13:01:22 +0900

    DP - 1D 746. Min Cost Climbing Stairs

--- DP-1D/746_Min_Cost_Climbing_Stairs.h
+++ DP-1D/746_Min_Cost_Climbing_Stairs.h
@@ -25,3 +25,49 @@ public:
 
 	}
 };
+
+class Solution {
+
+	int jump(vector<int>& cost, int n, int sum)
+	{
+		if (0 < memo.count(n))
+		{
+			memo[n] = std::min(memo[n], sum);
+		}
+		else
+		{
+			memo[n] = sum;
+		}
+		
+		if (cost.size() <= n + 1 || cost.size() <= n + 2)
+		{
+			return sum;
+		}
+
+		int ret = std::min(
+			jump(cost, n + 1, cost[n + 1] + sum),
+			jump(cost, n + 2, cost[n + 2] + sum)
+		);
+
+		if (0 < memo.count(n))
+		{
+			memo[n] = std::min(memo[n], ret);
+		}
+		else
+		{
+			memo[n] = ret;
+		}
+		
+
+		return memo[n];
+	};
+
+	unordered_map<int, int> memo;
+
+public:
+	int minCostClimbingStairs(vector<int>& cost) {
+
+		return jump(cost, -1, 0);
+
+	}
+};
