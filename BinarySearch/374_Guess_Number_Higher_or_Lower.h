d01d8024e266fc93ac03ad392b0ad7034b77ae86
ordinarydennis <ordinarydennis@gmail.com>
2024-08-03 22:51:52 +0900

    Binary Search - 374. Guess Number Higher or Lower(9m)

--- BinarySearch/374_Guess_Number_Higher_or_Lower.h
+++ BinarySearch/374_Guess_Number_Higher_or_Lower.h
@@ -34,3 +34,40 @@ public:
 		return -1;
 	}
 };
+
+/**
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+class Solution {
+
+public:
+	int guessNumber(int n) {
+
+		int end = n;
+		int begin = 1;
+
+		int mid = begin + (end - begin) / 2;
+
+		while (begin <= end)
+		{
+			int result = guess(mid);
+
+			if (0 == result)
+				break;
+			else if (-1 == result)
+				end = mid - 1;
+			else
+				begin = mid + 1;
+
+			mid = begin + (end - begin) / 2;
+		}
+
+		return mid;
+	}
+};
