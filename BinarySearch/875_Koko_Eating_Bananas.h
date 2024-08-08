438069bf9ad4b22e1ec3b448a50a3db9ea48172a
ordinarydennis <ordinarydennis@gmail.com>
2024-08-08 12:59:44 +0900

    Binary Search - 875. Koko Eating Bananas

--- BinarySearch/875_Koko_Eating_Bananas.h
+++ BinarySearch/875_Koko_Eating_Bananas.h
@@ -36,3 +36,48 @@ public:
 	}
 };
 
+
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+       
+		int d = piles.size() / h;
+
+		int p = d * *max_element(piles.begin(), piles.end());
+
+		
+
+		int ret = INT_MAX;
+
+		int total = accumulate(piles.begin(), piles.end(), 0);
+
+		while (1)
+		{
+			int count = 0;
+			int psum = 0;
+
+			for (int n : piles)
+			{
+				while (0 < n)
+				{
+					psum += (0 < n - p) ? p : n;
+
+					n -= p;
+					
+					count++;
+				}
+			}
+
+			p--;
+
+			ret = min(ret, count);
+
+			if (psum <= total)
+			{
+				break;
+			}
+		}
+
+		return ret;
+    }
+};
