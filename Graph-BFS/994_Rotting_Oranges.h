03ea81f862d78a422f1c9af7c2eabc7e9394fcbc
ordinarydennis <ordinarydennis@gmail.com>
2024-08-13 18:15:25 +0900

    Graphs - BFS - 994. Rotting Oranges(try again)

--- Graph-BFS/994_Rotting_Oranges.h
+++ Graph-BFS/994_Rotting_Oranges.h
@@ -77,3 +77,91 @@ public:
 		return ret;
 	}
 };
+
+class Solution {
+public:
+	int orangesRotting(vector<vector<int>>& grid) {
+
+		queue<array<int, 2>> q;
+
+		int freshOrangeCount = 0;
+
+		for (int y = 0; y < grid.size(); y++)
+		{
+			for (int x = 0; x < grid[0].size(); x++)
+			{
+				if(2 == grid[y][x])
+				{ 
+					q.push({ y, x });
+				}
+				else if (1 == grid[y][x])
+				{
+					freshOrangeCount++;
+				}
+			}
+		}
+
+		if (0 == freshOrangeCount)
+		{
+			return 0;
+		}
+
+		int ret = 0;
+
+		vector<vector<int>> dir = {
+			{1, 0},
+			{-1, 0},
+			{0, 1},
+			{0, -1},
+		};
+
+
+		int maxX = grid[0].size() - 1;
+		int maxY = grid.size() - 1;
+
+		while (q.size())
+		{
+			bool b = false;
+
+			int size = q.size();
+
+			for (int i = 0; i < size; i++)
+			{
+
+				auto o = q.front();
+
+				q.pop();
+
+				int y = o[0];
+				int x = o[1];
+
+				for (auto d : dir)
+				{
+					int dy = d[0] + y;
+					int dx = d[1] + x;
+
+					if (dy < 0 || maxY < dy)
+					{
+						continue;
+					}
+
+					if (dx < 0 || maxX < dx)
+					{
+						continue;
+					}
+
+					if (1 == grid[dy][dx])
+					{
+						grid[dy][dx] = 2;
+						q.push({ dy, dx });
+						freshOrangeCount--;
+					}
+				}
+			}
+
+			ret++;
+		}
+
+		return 0 < freshOrangeCount ? -1 : ret - 1;
+	}
+};
