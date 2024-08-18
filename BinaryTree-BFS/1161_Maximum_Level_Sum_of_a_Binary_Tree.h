ea17d19aad12b39f6920ab83ae67fc0bf42e903f
ordinarydennis <ordinarydennis@gmail.com>
2024-08-19 07:53:04 +0900

    Binary Search Tree - 1161. Maximum Level Sum of a Binary Tree(10m)

--- BinaryTree-BFS/1161_Maximum_Level_Sum_of_a_Binary_Tree.h
+++ BinaryTree-BFS/1161_Maximum_Level_Sum_of_a_Binary_Tree.h
@@ -101,3 +101,66 @@ public:
 		return ret;
 	}
 };
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+	int maxLevelSum(TreeNode* root) {
+
+		queue<TreeNode*> q;
+
+		q.push(root);
+
+		int level = 1;
+		int max = INT_MIN;
+
+		int ret = 0;
+
+		while (q.size())
+		{
+			int s = q.size();
+
+			int sum = 0;
+
+			for (int i = 0; i < s; i++)
+			{
+				auto* node = q.front();
+
+				q.pop();
+
+				sum += node->val;
+	
+				if (node->left)
+				{
+					q.push(node->left);
+				}
+
+				if (node->right)
+				{
+					q.push(node->right);
+				}
+			}
+
+			if (max < sum)
+			{
+				max = sum;
+				ret = level;
+			}
+
+			level++;
+		}
+
+		return ret;
+	}
+};
