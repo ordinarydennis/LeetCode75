58e90d2d4ceea374c2f69f67d2694c91c2bc7272
ordinarydennis <ordinarydennis@gmail.com>
2024-08-18 17:30:34 +0900

    Binary Search Tree - 700. Search in a Binary Search Tree(6m) - 450. Delete Node in a BST Binary Tree - BFS - 199. Binary Tree Right Side View(7m)

--- BinarySearchTree/450_Delete_Node_in_a_BST.h
+++ BinarySearchTree/450_Delete_Node_in_a_BST.h
@@ -120,3 +120,131 @@ public:
 
 	}
 };
+
+
+
+class TreeNode {
+public:
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+class Solution {
+public:
+	/*
+	One step right and then always left
+	*/
+	int successor(TreeNode* root) {
+		root = root->right;
+		while (root->left != nullptr) root = root->left;
+		return root->val;
+	}
+
+	/*
+	One step left and then always right
+	*/
+	int predecessor(TreeNode* root) {
+		root = root->left;
+		while (root->right != nullptr) root = root->right;
+		return root->val;
+	}
+
+	TreeNode* deleteNode(TreeNode* root, int key) {
+		if (root == nullptr) return nullptr;
+
+		// delete from the right subtree
+		if (key > root->val) 
+			root->right = deleteNode(root->right, key);
+		// delete from the left subtree
+		else if (key < root->val) 
+			root->left = deleteNode(root->left, key);
+		// delete the current node
+		else 
+		{
+			// the node is a leaf
+			if (root->left == nullptr && root->right == nullptr) {
+				delete root;
+				root = nullptr;
+			}
+			// the node is not a leaf and has a right child
+			else if (root->right != nullptr) {
+				root->val = successor(root);
+				root->right = deleteNode(root->right, root->val);
+			}
+			// the node is not a leaf, has no right child, and has a left child    
+			else {
+				root->val = predecessor(root);
+				root->left = deleteNode(root->left, root->val);
+			}
+		}
+		return root;
+	}
+};
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+
+
+public:
+	TreeNode* deleteNode(TreeNode* root, int key) {
+
+		if (!root)
+		{
+			return nullptr;
+		}
+
+		if (root->val == key)
+		{
+			if (nullptr == root->left && nullptr == root->right)
+			{
+				delete root;
+				return nullptr;
+			}
+			else if (root->right)
+			{
+				auto r = root->right;
+
+				while (r->left)
+					r = r->left;
+
+				root->val = r->val;
+
+				root->right = deleteNode(root->right, r->val);
+			}
+
+			else if (root->left)
+			{
+				auto l = root->left;
+				
+				while (l->right)
+					l = l->right;
+
+				root->val = l->val;
+
+				root->left = deleteNode(root->left, l->val);
+			}
+		}
+		else
+		{
+			if (root->val < key)
+				root->right = deleteNode(root->right, key);
+			else
+				root->left = deleteNode(root->left, key);		
+		}
+
+		return root;
+	}
+};
