ff0842cc1f629e32a04840ef068876c221f6c416
ordinarydennis <ordinarydennis@gmail.com>
2024-08-19 12:47:24 +0900

    Binary Search Tree - 1161. Maximum Level Sum of a Binary Tree(solved again as dfs, 14m)

--- BinaryTree-BFS/1161_Maximum_Level_Sum_of_a_Binary_Tree.h
+++ BinaryTree-BFS/1161_Maximum_Level_Sum_of_a_Binary_Tree.h
@@ -140,7 +140,7 @@ public:
 				q.pop();
 
 				sum += node->val;
-	
+
 				if (node->left)
 				{
 					q.push(node->left);
@@ -159,6 +159,60 @@ public:
 			}
 
 			level++;
+		}
+
+		return ret;
+	}
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+
+	void dfs(TreeNode* node, int level, unordered_map<int, int>& m)
+	{
+		m[level] += node->val;
+
+		if (node->left)
+		{
+			dfs(node->left, level + 1, m);
+		}
+
+		if (node->right)
+		{
+			dfs(node->right, level + 1, m);
+		}
+	}
+
+public:
+	int maxLevelSum(TreeNode* root) {
+
+		unordered_map<int, int> m;
+
+		dfs(root, 1, m);
+
+		int max = INT_MIN;
+
+		int ret = INT_MAX;
+
+		for (auto& [level, sum] : m)
+		{
+			if (sum < max)
+				continue;
+
+			if (max < sum)
+				max = sum;
+
+			ret = level;
 		}
 
 		return ret;
