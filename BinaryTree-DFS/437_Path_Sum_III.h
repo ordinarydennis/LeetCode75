8d9eca62010279a73c6cd2d76bc63d4dfc2abfef
ordinarydennis <ordinarydennis@gmail.com>
2024-08-21 07:10:46 +0900

    Binary Tree - DFS - 437. Path Sum III(read editorial, neet to retry) https://leetcode.com/problems/path-sum-iii/editorial/?envType=study-plan-v2&envId=leetcode-75

--- BinaryTree-DFS/437_Path_Sum_III.h
+++ BinaryTree-DFS/437_Path_Sum_III.h
@@ -1,4 +1,4 @@
-/**
+﻿/**
  * Definition for a binary tree node.
  * struct TreeNode {
  *     int val;
@@ -38,4 +38,98 @@ public:
 
 		return ret;
 	}
+};
+
+
+#include <unordered_map>
+#include <vector>
+
+class Solution {
+public:
+    int subarraySum(std::vector<int>& nums, int k) {
+        int count = 0;
+        int currSum = 0;
+        std::unordered_map<int, int> prefixSumCount;
+
+        for (int num : nums) {
+            // Update the current prefix sum
+            currSum += num;
+
+            // Situation 1:
+            // Continuous subarray starts from the beginning of the array
+            if (currSum == k) {
+                count++;
+            }
+
+            // Situation 2:
+            // The number of times the curr_sum − k has occurred already,
+            // determines the number of times a subarray with sum k
+            // has occurred up to the current index
+            if (prefixSumCount.find(currSum - k) != prefixSumCount.end()) {
+                count += prefixSumCount[currSum - k];
+            }
+
+            // Add the current sum to the map or update existing count
+            prefixSumCount[currSum]++;
+        }
+
+        return count;
+    }
+};
+
+#include <unordered_map>
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+class Solution {
+private:
+    int count = 0;
+    long long k;
+    std::unordered_map<long long, int> prefixSumCount;
+
+    void preorder(TreeNode* node, long long currSum) {
+        if (node == null)
+            return;
+
+        // Update the current prefix sum
+        currSum += node->val;
+
+        // Check if current prefix sum equals the desired sum
+        if (currSum == k)
+            count++;
+
+        // Check if there is a prefix sum that up to current node minus k exists
+        count += prefixSumCount[currSum - k];
+
+        // Add the current sum into the map to use it for this node's children
+        prefixSumCount[currSum]++;
+
+        // Process the left subtree
+        preorder(node->left, currSum);
+
+        // Process the right subtree
+        preorder(node->right, currSum);
+
+        // After returning from recursion, decrement the count of currSum in the map,
+        // as we move back up the tree
+        prefixSumCount[currSum]--;
+        if (prefixSumCount[currSum] == 0) {
+            prefixSumCount.erase(currSum);
+        }
+    }
+
+public:
+    int pathSum(TreeNode* root, int sum) {
+        k = sum;
+        
+        preorder(root, 0);
+
+        return count;
+    }
 };
