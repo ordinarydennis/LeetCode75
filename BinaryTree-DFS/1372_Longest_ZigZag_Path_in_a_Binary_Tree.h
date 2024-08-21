dab047ade03765c8d061a5b9c1db9708509a3b2a
ordinarydennis <ordinarydennis@gmail.com>
2024-08-22 08:09:58 +0900

    Binary Tree - DFS - 1372. Longest ZigZag Path in a Binary Tree

--- BinaryTree-DFS/1372_Longest_ZigZag_Path_in_a_Binary_Tree.h
+++ BinaryTree-DFS/1372_Longest_ZigZag_Path_in_a_Binary_Tree.h
@@ -11,27 +11,124 @@
  */
 class Solution {
 public:
-    int pathLength = 0;
-    void dfs(TreeNode* node, bool goLeft, int steps) {
-        if (node == nullptr) {
-            return;
-        }
-        pathLength = max(pathLength, steps);
-        if (goLeft) {
-            dfs(node->left, false, steps + 1);
-            dfs(node->right, true, 1);
-        }
-        else {
-            dfs(node->left, false, 1);
-            dfs(node->right, true, steps + 1);
-        }
-    }
+	int pathLength = 0;
+	void dfs(TreeNode* node, bool goLeft, int steps) {
+		if (node == nullptr) {
+			return;
+		}
+		pathLength = max(pathLength, steps);
+		if (goLeft) {
+			dfs(node->left, false, steps + 1);
+			dfs(node->right, true, 1);
+		}
+		else {
+			dfs(node->left, false, 1);
+			dfs(node->right, true, steps + 1);
+		}
+	}
 
-    int longestZigZag(TreeNode* root) {
-        dfs(root->left, false, 1);
-        dfs(root->right, true, 1);
-        return pathLength;
-    }
+	int longestZigZag(TreeNode* root) {
+		dfs(root->left, false, 1);
+		dfs(root->right, true, 1);
+		return pathLength;
+	}
 };
 
 //https://leetcode.com/problems/longest-zigzag-path-in-a-binary-tree/?envType=study-plan-v2&envId=leetcode-75
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+
+	bool dfs(TreeNode* root, bool isLeft)
+	{
+		if (!root)
+		{
+			return 0;
+		}
+
+		int lSize = dfs(root->left, true);
+
+		int rSize = dfs(root->right, false);
+
+		int total = 0;
+
+		if (isLeft)
+		{
+			total = rSize + 1;
+		}
+		else
+		{
+			total = lSize + 1;
+		}
+
+
+		ret = max(ret, total);
+
+
+		return total;
+	}
+
+	int ret = 0;
+
+public:
+	int longestZigZag(TreeNode* root) {
+
+		dfs(root->left, true);
+
+		dfs(root->right, false);
+
+		return ret;
+
+	}
+};
+
+
+
+class Solution {
+
+	void dfs(TreeNode* root, bool isLeft, int sum)
+	{
+		if (!root)
+		{
+			return;
+		}
+
+		if (isLeft)
+		{
+			dfs(root->left, true, 0);
+			dfs(root->right, false, sum + root->val);
+		}
+		else
+		{
+			dfs(root->right, false, 0);
+			dfs(root->left, true, sum + root->val);
+		}
+
+		ret = max(ret, sum + root->val);
+	}
+
+	int ret = 0;
+
+public:
+	int longestZigZag(TreeNode* root) {
+
+		dfs(root->left, true, 0);
+
+		dfs(root->right, false, 0);
+
+		return ret;
+	}
+};
