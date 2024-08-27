08a80cbe90690df31324d4283798bca3a9995e7f
ordinarydennis <ordinarydennis@gmail.com>
2024-08-27 14:21:16 +0900

    Stack - 2390. Removing Stars From a String(using stack, two pointer)

--- Stack/2390_Removing_Stars_From_a_String.h
+++ Stack/2390_Removing_Stars_From_a_String.h
@@ -11,3 +11,52 @@ public:
 
 	}
 };
+
+//stack
+class Solution {
+public:
+	string removeStars(string s) {
+
+		std::stack<char> st;
+
+		for (char c : s)
+			('*' == c) ? st.pop() : st.push(c);
+
+		string ret;
+
+		while (st.size())
+		{
+			ret.push_back(st.top());
+			st.pop();
+		}
+
+		reverse(ret.begin(), ret.end());
+
+		return ret;
+	}
+};
+
+//two pointer
+class Solution {
+public:
+	string removeStars(string s) {
+		vector<char> ch(s.size());
+		int j = 0;
+
+		for (int i = 0; i < s.size(); i++) {
+			if (s[i] == '*') {
+				j--;
+			}
+			else {
+				ch[j++] = s[i];
+			}
+		}
+
+		string answer = "";
+		for (int i = 0; i < j; i++) {
+			answer.push_back(ch[i]);
+		}
+
+		return answer;
+	}
+};
