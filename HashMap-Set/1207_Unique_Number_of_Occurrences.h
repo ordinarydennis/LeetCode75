0ccb28da92154dbaf2fae89c412b219cd5a4765f
ordinarydennis <ordinarydennis@gmail.com>
2024-08-29 13:11:41 +0900

    Hash Map / Set - 1207. Unique Number of Occurrences(5m) - 1657. Determine if Two Strings Are Close(8m)

--- HashMap-Set/1207_Unique_Number_of_Occurrences.h
+++ HashMap-Set/1207_Unique_Number_of_Occurrences.h
@@ -20,3 +20,32 @@ public:
 		return true;
 	}
 };
+
+class Solution {
+public:
+	bool uniqueOccurrences(vector<int>& arr) {
+
+		std::unordered_map<int, int> m;
+
+		for (int i = 0; i < arr.size(); i++)
+		{
+			m[arr[i]]++;
+		}
+
+
+		std::unordered_set<int> oSet;
+
+		for (auto& [n, o] : m)
+		{
+			if (oSet.count(o))
+			{
+				return false;
+			}
+
+			oSet.insert(o);
+		}
+
+		return true;
+
+	}
+};
--- HashMap-Set/1657_Determine_if_Two_Strings_Are_Close.h
+++ HashMap-Set/1657_Determine_if_Two_Strings_Are_Close.h
@@ -34,3 +34,78 @@ public:
 		return countS1 == countS2 && nCountV1 == nCountV2;
 	}
 };
+
+class Solution {
+public:
+	bool closeStrings(string word1, string word2) {
+
+		if (word1.size() != word2.size())
+			return false;
+
+		set<char> cSet1;
+		unordered_map<char, int> map1;
+		for (int i = 0;i < word1.size(); i++)
+		{
+			cSet1.insert(word1[i]);
+			map1[word1[i]]++;
+		}
+
+		vector<int> v1;
+		for (auto [_, o] : map1)
+		{
+			v1.push_back(o);
+		}
+
+		sort(v1.begin(), v1.end());
+
+		set<char> cSet2;
+		unordered_map<char, int> map2;
+		for (int i = 0;i < word2.size(); i++)
+		{
+			cSet2.insert(word2[i]);
+			map2[word2[i]]++;
+		}
+
+		vector<int> v2;
+		for (auto [_, o] : map2)
+		{
+			v2.push_back(o);
+		}
+		sort(v2.begin(), v2.end());
+
+
+		return cSet1 == cSet2 && v1 == v2;
+	}
+};
+
+class Solution {
+public:
+	bool closeStrings(string word1, string word2) {
+		if (word1.size() != word2.size())
+			return false;
+
+		vector<int> m1('z' - 'a' + 1, 0);
+		vector<int> m2('z' - 'a' + 1, 0);
+
+		for (int i = 0; i < word1.size(); i++)
+		{
+			m1[word1[i] - 'a']++;
+			m2[word2[i] - 'a']++;
+		}
+
+		for (int i = 0; i < 'z' - 'a' + 1; i++)
+		{
+			if (m1[i] == 0 && m2[i] > 0 ||
+				m2[i] == 0 && m1[i] > 0
+				)
+			{
+				return false;
+			}
+		}
+
+		sort(m1.begin(), m1.end());
+		sort(m2.begin(), m2.end());
+
+		return m1 == m2;
+	}
+};
