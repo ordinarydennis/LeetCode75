5b23764b2d0d572d8004b21c7dfecf16cac8dff0
ordinarydennis <ordinarydennis@gmail.com>
2024-09-02 08:06:28 +0900

    Two Pointers - 283. Move Zeroes(8m, Read Editorial)

--- TwoPointers/283_Move_Zeroes.h
+++ TwoPointers/283_Move_Zeroes.h
@@ -17,3 +17,61 @@ public:
 		}
 	}
 };
+
+class Solution {
+public:
+	void moveZeroes(vector<int>& nums) {
+
+		vector<int> ret;
+
+		int eCount = 0;
+
+		for (int i = 0; i < nums.size(); i++)
+		{
+			if (0 != nums[i])
+			{
+				ret.push_back(nums[i]);
+			}
+			else
+			{
+				eCount++;
+			}
+		}
+
+		for (int i = 0; i < eCount; i++)
+			ret.push_back(0);
+
+		nums = std::move(ret);
+	}
+};
+
+
+
+class Solution {
+public:
+	void moveZeroes(vector<int>& nums) {
+
+		int point = 0;
+
+		for (int i = 0; i < nums.size(); i++)
+		{
+			if (0 != nums[i])
+				nums[point++] = nums[i];
+		}
+
+		for (int i = point; i < nums.size(); i++)
+			nums[i] = 0;
+	}
+};
+
+class Solution {
+public:
+	void moveZeroes(vector<int>& nums) {
+		
+		for (int point = 0, i = 0; i < nums.size(); i++)
+		{
+			if (0 != nums[i])
+				std::swap(nums[point++], nums[i]);
+		}
+	}
+};
