f9190cde558ff71f5c4a9a3aca7d37f7603c0e4e
ordinarydennis <ordinarydennis@gmail.com>
2024-09-03 08:04:17 +0900

    Two Pointers 11. Container With Most Water(8m) 1679. Max Number of K-Sum Pairs(4m, read editorial)

--- TwoPointers/11_Container_With_Most_Water.h
+++ TwoPointers/11_Container_With_Most_Water.h
@@ -20,3 +20,30 @@ public:
 
 	}
 };
+
+
+class Solution {
+public:
+	int maxArea(vector<int>& height) {
+
+		int left = 0;
+
+		int right = height.size() - 1;
+
+		int ret = 0;
+
+		while (left < right)
+		{
+			int h = height[left] < height[right] ? height[left] : height[right];
+			
+			ret = max(ret, h * (right - left));
+
+			if (height[left] < height[right])
+				left++;				
+			else
+				right--;
+		}
+		
+		return ret;
+	}
+};
--- TwoPointers/1679_Max_Number_of_K-Sum_Pairs.h
+++ TwoPointers/1679_Max_Number_of_K-Sum_Pairs.h
@@ -29,3 +29,57 @@ public:
 
 	}
 };
+
+
+class Solution {
+public:
+	int maxOperations(vector<int>& nums, int k) {
+
+		sort(nums.begin(), nums.end());
+
+		int left = 0;
+		int right = nums.size() - 1;
+		int ret = 0;
+
+		while (left < right)
+		{
+			if (k == nums[left] + nums[right])
+			{
+				left++;
+				right--;
+				ret++;
+				continue;
+			}
+
+			(k < nums[left] + nums[right]) ? right-- : left++;
+		}
+
+		return ret;
+	}
+};
+
+
+class Solution {
+public:
+	int maxOperations(vector<int>& nums, int k) {
+
+		unordered_map<int, int> m;
+
+		int count = 0;
+
+		for (int i = 0; i < nums.size(); i++)
+		{
+			if (0 < m[k - nums[i]])
+			{
+				m[k - nums[i]]--;
+				count++;
+			}
+			else
+			{
+				m[nums[i]]++;
+			}
+		}
+
+		return count;
+	}
+};
