ec20e1a0da2b874e3edc2a35bf6b4a81ecea66d8
ordinarydennis <ordinarydennis@gmail.com>
2024-09-02 19:31:45 +0900

    Two Pointers - 392. Is Subsequence(4m)

--- TwoPointers/392_Is_Subsequence.h
+++ TwoPointers/392_Is_Subsequence.h
@@ -18,3 +18,18 @@ public:
 
 	}
 };
+
+class Solution {
+public:
+	bool isSubsequence(string s, string t) {
+
+		int sIndex = 0;
+
+		for (int i = 0; i < t.size(); i++)
+		{
+			if (s[sIndex] == t[i]) sIndex++;
+		}
+
+		return s.size() == sIndex;
+	}
+};
