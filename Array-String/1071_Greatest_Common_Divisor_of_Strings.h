91b3a82a2efccd4d3ad72c536cfd81acb17ea8aa
ordinarydennis <ordinarydennis@gmail.com>
2024-09-03 13:13:17 +0900

    String / Array - 1768. Merge Strings Alternately(2m) - 1071. Greatest Common Divisor of Strings

--- Array-String/1071_Greatest_Common_Divisor_of_Strings.h
+++ Array-String/1071_Greatest_Common_Divisor_of_Strings.h
@@ -28,3 +28,49 @@ public:
 		return "";
 	}
 };
+
+class Solution {
+
+	bool checkDiv(string str, string common)
+	{
+		int size = common.size();
+
+		for (int b = 0; b < str.size();)
+		{
+			string sub2 = str.substr(b, b + size);
+
+			if (sub2 != common)
+			{
+				return false;
+			}
+
+			b += size;
+		}
+
+		return true;
+	}
+
+
+public:
+	string gcdOfStrings(string str1, string str2) {
+
+		string ret;
+
+		int maxSize = (str1.size() < str2.size()) ? str1.size() : str2.size();
+
+		for (int i = 0; i < maxSize; i++)
+		{
+			for (int s = 1; s < maxSize; s++)
+			{
+				auto common = str1.substr(i, s);
+
+				if (checkDiv(str1, common) && checkDiv(str2, common))
+				{
+					ret = common;
+				}
+			}
+		}
+
+		return ret;
+	}
+};
--- Array-String/1768_Merge_Strings_Alternately.h
+++ Array-String/1768_Merge_Strings_Alternately.h
@@ -1,29 +1,56 @@
 class Solution {
 public:
-    string mergeAlternately(string word1, string word2) {
+	string mergeAlternately(string word1, string word2) {
 
-        int max = std::max(word1.size(), word2.size());
+		int max = std::max(word1.size(), word2.size());
 
-        string ret;
+		string ret;
 
-        for (int i = 0; i < max; i++)
-        {
-            if (i < word1.size())
-            {
-                ret.push_back(word1[i])''
-            }
+		for (int i = 0; i < max; i++)
+		{
+			if (i < word1.size())
+			{
+				ret.push_back(word1[i])''
+			}
 
-            if (i < word2.size())
-            {
-                ret.push_back(word1[i]);;
-            }
-        }
+			if (i < word2.size())
+			{
+				ret.push_back(word1[i]);;
+			}
+		}
 
-        return ret;
-    }
+		return ret;
+	}
 };
 
 //Approach 1: One Pointer
 //If n is the greater of word1 and word2. 
 //Time Complexity O(n)
 //Space Complexity O(1)
+
+class Solution {
+public:
+	string mergeAlternately(string word1, string word2) {
+
+		int a = 0;
+		int b = 0;
+
+		string ret;
+
+		while (a < word1.size() || b < word2.size())
+		{
+			if (a < word1.size())
+			{
+				ret += word1[a++];
+			}
+
+			if (b < word2.size())
+			{
+				ret += word2[b++];
+			}
+		}
+
+		return ret;
+
+	}
+};
