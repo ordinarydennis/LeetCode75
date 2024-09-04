6db87d5a8614b7e7cfde953bcfcdb37ffce5676f
ordinarydennis <ordinarydennis@gmail.com>
2024-09-04 19:08:15 +0900

    String / Array - 1071. Greatest Common Divisor of Strings(read editorial) https://leetcode.com/problems/greatest-common-divisor-of-strings/editorial/?envType=study-plan-v2&envId=leetcode-75

--- Array-String/1071_Greatest_Common_Divisor_of_Strings.h
+++ Array-String/1071_Greatest_Common_Divisor_of_Strings.h
@@ -74,3 +74,87 @@ public:
 		return ret;
 	}
 };
+
+//https://leetcode.com/problems/greatest-common-divisor-of-strings/editorial/?envType=study-plan-v2&envId=leetcode-75
+
+class Solution {
+public:
+	bool valid(string str1, string str2, int k) {
+
+		int len1 = str1.size(), len2 = str2.size();
+
+		if (len1 % k > 0 || len2 % k > 0) {
+			return false;
+		}
+		else
+		{
+			string base = str1.substr(0, k);
+
+			int n1 = len1 / k, n2 = len2 / k;
+
+			return str1 == joinWords(base, n1) && str2 == joinWords(base, n2);
+		}
+	}
+
+	string joinWords(string str, int k) {
+
+		string ans = "";
+
+		for (int i = 0; i < k; ++i)
+		{
+			ans += str;
+		}
+
+		return ans;
+	}
+
+
+	string gcdOfStrings(string str1, string str2) {
+
+		int len1 = str1.length(), len2 = str2.length();
+
+		for (int i = min(len1, len2); i >= 1; --i)
+		{
+			if (valid(str1, str2, i))
+			{
+				return str1.substr(0, i);
+			}
+		}
+		return "";
+	}
+};
+
+
+
+class Solution {
+public:
+	string gcdOfStrings(string str1, string str2) {
+
+		int size1 = str1.size();
+		int size2 = str2.size();
+
+		int commonSize = std::min(size1, size2);
+
+		for (int size = commonSize; 0 < size; size--)
+		{
+			if (0 != size1 % size || 0 != size2 % size)
+				continue;
+
+			string commonStr = str1.substr(0, size);
+			
+			string substr1;
+			for (int i = 0; i < size1 / size; i++)
+				substr1 += commonStr;
+			
+			string substr2;
+			for (int i = 0; i < size2 / size; i++)
+				substr2 += commonStr;
+
+			if (str1 == substr1 && str2 == substr2)
+				return commonStr;
+
+		}
+			
+		return "";
+	}
+};
