2b3413cc9d89b8362717116cbf4d933a8d01dfd6
ordinarydennis <ordinarydennis@gmail.com>
2024-09-08 20:07:47 +0900

    String / Array - 334. Increasing Triplet Subsequence(read editorial)

--- Array-String/334_Increasing_Triplet_Subsequence.h
+++ Array-String/334_Increasing_Triplet_Subsequence.h
@@ -1,22 +1,113 @@
 class Solution {
 public:
-    bool increasingTriplet(std::vector<int>& nums) {
-        
-        int first_num = INT_MAX;
-        
-        int second_num = INT_MAX;
-        
-        for (int n : nums) {
-            if (n <= first_num) {
-                first_num = n;
-            }
-            else if (n <= second_num) {
-                second_num = n;
-            }
-            else {
-                return true;
-            }
-        }
-        return false;
-    }
+	bool increasingTriplet(std::vector<int>& nums) {
+		
+		int first_num = INT_MAX;
+		
+		int second_num = INT_MAX;
+		
+		for (int n : nums) {
+			if (n <= first_num) {
+				first_num = n;
+			}
+			else if (n <= second_num) {
+				second_num = n;
+			}
+			else {
+				return true;
+			}
+		}
+		return false;
+	}
+};
+
+
+class Solution {
+public:
+	bool increasingTriplet(vector<int>& nums) {
+
+		for (int a = 0; a < nums.size() - 2; a++)
+		{
+			for (int b = a + 1; b < nums.size() - 1; b++)
+			{
+				for (int c = b + 1; c < nums.size(); c++)
+				{
+					if (nums[a] < nums[b] && nums[b] < nums[c])
+					{
+						return true;
+					}
+				}
+			}
+		}
+
+		return false;
+	}
+};
+
+class Solution {
+public:
+	bool increasingTriplet(vector<int>& nums) {
+
+		vector<pair<int, int>> pList;
+
+		for (int i = 0; i < nums.size(); i++)
+		{
+			pList.emplace_back(pair<int, int>{ nums[i], i });
+		}
+
+		sort(pList.begin(), pList.end(),
+			[](const pair<int, int>& l, const pair<int, int>& r) {
+
+				if (l.first == r.first)
+					return l.second < r.second;
+
+				return l.first < r.first;
+			}
+		);
+
+		int max = -1;
+		int count = 0;
+
+		for (const auto& p : pList)
+		{
+			if (max < p.second)
+			{
+				max = p.second;
+				count++;
+				if (3 == count)
+				{
+					break;
+				}
+			}
+		}
+
+		return count == 3;
+	}
+};
+
+class Solution {
+public:
+	bool increasingTriplet(vector<int>& nums) {
+
+		int first = INT_MAX;
+		int second = INT_MAX;
+
+		for (int n : nums)
+		{
+			if (n <= first)
+			{
+				first = n;
+			}
+			else if (n <= second)
+			{
+				second = n;
+			}
+			else
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}
 };
