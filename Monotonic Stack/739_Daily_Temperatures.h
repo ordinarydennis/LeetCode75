85b6aa5724655f6b5be4580c435f5061757278e8
ordinarydennis <ordinarydennis@gmail.com>
2024-09-10 22:34:28 +0900

    Monotonic Stack - 739. Daily Temperatures(10m)

--- Monotonic Stack/739_Daily_Temperatures.h
+++ Monotonic Stack/739_Daily_Temperatures.h
@@ -20,3 +20,42 @@ public:
 		return ret;
 	}
 };
+
+class Solution {
+public:
+	vector<int> dailyTemperatures(vector<int>& temperatures) {
+
+		stack<pair<int, int>> st;
+
+		vector<int> ret(temperatures.size());
+
+		for (int i = 0; i < temperatures.size(); i++)
+		{
+			if (0 == st.size())
+			{
+				st.push({ i, temperatures[i] });
+				continue;
+			}
+
+			while (st.size())
+			{
+				auto t = st.top;
+
+				if (t.second < temperatures[i])
+				{
+					ret[t.first] = i - t.first;
+					st.pop();
+				}
+				else
+				{
+					break;
+				}
+			}
+
+			st.push({ i , temperatures[i] });
+
+		}
+
+		return ret;
+	}
+};
