5316411c45fcd7a8f2bfeddd6c6da64a3a80b833
ordinarydennis <ordinarydennis@gmail.com>
2024-09-12 12:51:59 +0900

    Intervals - 452. Minimum Number of Arrows to Burst Balloons(14m)

--- Intervals/452_Minimum_Number_of_Arrows_to_Burst_Balloons.h
+++ Intervals/452_Minimum_Number_of_Arrows_to_Burst_Balloons.h
@@ -31,3 +31,37 @@ public:
 };
 
 //When you want to know if it's overlapping, you need to sort the list of locations as end points
+
+
+class Solution {
+public:
+	int findMinArrowShots(vector<vector<int>>& points) {
+
+		sort(points.begin(), points.end(), [](vector<int>& l , vector<int>& r) {
+			
+			if (l[1] == r[1])
+			{
+				return l[0] < l[0];
+			}
+
+			return l[1] < r[1];
+
+			}
+		);
+
+		int end = points[0][1];
+
+		int ret = 1;
+
+		for (int i = 1; i < points.size(); i++)
+		{
+			if (end < points[i][0])
+			{
+				end = points[i][1];
+				ret++;
+			}
+		}
+
+		return ret;
+	}
+};
