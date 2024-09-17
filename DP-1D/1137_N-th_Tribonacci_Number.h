b542d021a43cd583a04e70b1dd3c45abbe1dfb2c
ordinarydennis <ordinarydennis@gmail.com>
2024-09-17 20:22:28 +0900

    DP - 1D - 1137. N-th Tribonacci Number(5m) - 746. Min Cost Climbing Stairs(8m, dfs, bfs)

--- DP-1D/1137_N-th_Tribonacci_Number.h
+++ DP-1D/1137_N-th_Tribonacci_Number.h
@@ -117,3 +117,90 @@ public:
 		return ret;
 	}
 };
+
+
+class Solution {
+public:
+	int tribonacci(int n) {
+
+		if (0 == n)
+			return 0;
+
+		if (n <= 2)
+			return 1;
+
+		int t0 = 0;
+		int t1 = 1;
+		int t2 = 1;
+
+		int ret = 0;
+
+		while (n - 2)
+		{
+			ret = t0 + t1 + t2;
+			t0 = t1;
+			t1 = t2;
+			t2 = ret;
+			n--;
+		}
+
+		return ret;
+	}
+};
+
+class Solution {
+public:
+	int tribonacci(int n) {
+
+		if (0 == n)
+			return 0;
+
+		if (n <= 2)
+
+			return 1;
+		vector<int> list(n + 1);
+
+		list[0] = 0;
+		list[1] = 1;
+		list[2] = 1;
+
+		int ret = 0;
+
+		for (int i = 3; i <= n; i++)
+		{
+			list[i] = list[i - 1] + list[i - 2] + list[i - 3];
+		}
+
+		return list[n];
+
+	}
+};
+
+class Solution {
+
+	int dfs(int n)
+	{
+		if (memo.count(n))
+		{
+			return memo[n];
+		}
+
+		memo[n] = dfs(n - 1) + dfs(n - 2) + dfs(n - 3);
+
+		return memo[n];
+	}
+
+	unordered_map<int, int> memo = {
+		{0, 0},
+		{1, 1},
+		{2, 1}
+	};
+
+
+public:
+	int tribonacci(int n) {
+
+		return dfs(n);
+
+	}
+};
--- DP-1D/746_Min_Cost_Climbing_Stairs.h
+++ DP-1D/746_Min_Cost_Climbing_Stairs.h
@@ -71,3 +71,136 @@ public:
 
 	}
 };
+
+class Solution {
+
+	int dfs(int n, vector<int>& cost)
+	{
+		if (memo.count(n))
+			return memo[n];
+
+		if (cost.size() <= n)
+			return 0;
+		
+		int minCost = std::min(
+			dfs(n + 1, cost),
+			dfs(n + 2, cost)
+		);
+
+
+		memo[n] = cost[n] + minCost;
+
+		return memo[n];
+	}
+
+
+	unordered_map<int, int> memo;
+
+public:
+	int minCostClimbingStairs(vector<int>& cost) {
+
+		return std::min(
+			dfs(0, cost), 
+			dfs(1, cost)
+		);
+
+	}
+};
+
+
+class Solution {
+	public int minCostClimbingStairs(int[] cost) {
+		// The array's length should be 1 longer than the length of cost
+		// This is because we can treat the "top floor" as a step to reach
+		int minimumCost[] = new int[cost.length + 1];
+
+		// Start iteration from step 2, since the minimum cost of reaching
+		// step 0 and step 1 is 0
+		for (int i = 2; i < minimumCost.length; i++) {
+			int takeOneStep = minimumCost[i - 1] + cost[i - 1];
+			int takeTwoSteps = minimumCost[i - 2] + cost[i - 2];
+			minimumCost[i] = Math.min(takeOneStep, takeTwoSteps);
+		}
+
+		// The final element in minimumCost refers to the top floor
+		return minimumCost[minimumCost.length - 1];
+	}
+}
+
+
+class Solution {
+	private HashMap<Integer, Integer> memo = new HashMap<Integer, Integer>();
+
+	public int minCostClimbingStairs(int[] cost) {
+		return minimumCost(cost.length, cost);
+	}
+
+	private int minimumCost(int i, int[] cost) {
+		// Base case, we are allowed to start at either step 0 or step 1
+		if (i <= 1) {
+			return 0;
+		}
+
+		// Check if we have already calculated minimumCost(i)
+		if (memo.containsKey(i)) {
+			return memo.get(i);
+		}
+
+		// If not, cache the result in our hash map and return it
+		int downOne = cost[i - 1] + minimumCost(i - 1, cost);
+		int downTwo = cost[i - 2] + minimumCost(i - 2, cost);
+		memo.put(i, Math.min(downOne, downTwo));
+		return memo.get(i);
+	}
+}
+
+
+class Solution {
+
+	int dfs(int n, vector<int>& cost)
+	{
+		if (memo.count(n))
+			return memo[n];
+
+		int m = std::min(
+			cost[n - 2] + dfs(n - 2, cost),
+			cost[n - 1] + dfs(n - 1, cost)
+		);
+		
+		return memo[n] = m;
+	}
+
+
+	unordered_map<int, int>  memo = { 
+		{0, 0}, 
+		{1, 0}, 
+	};
+
+
+public:
+	int minCostClimbingStairs(vector<int>& cost) {
+
+		return dfs(cost.size(), cost);
+
+	}
+};
+
+
+class Solution {
+
+	unordered_map<int, int> memo;
+
+public:
+	int minCostClimbingStairs(vector<int>& cost) {
+
+		for (int i = 2; i <= cost.size(); i++)
+		{
+			memo[i] = std::min(
+				memo[i - 1] + cost[i - 1],
+				memo[i - 2] + cost[i - 2]
+			);
+		}
+
+		return memo[cost.size()];
+	}
+};
