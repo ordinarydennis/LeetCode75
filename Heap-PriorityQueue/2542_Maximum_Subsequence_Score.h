6e457285ddaf1aa610d06c90b2b11c49e022096f
ordinarydennis <ordinarydennis@gmail.com>
2024-09-26 13:37:28 +0900

    Heap / Priority Queue - 2542. Maximum Subsequence Score(read editorial)

--- Heap-PriorityQueue/2542_Maximum_Subsequence_Score.h
+++ Heap-PriorityQueue/2542_Maximum_Subsequence_Score.h
@@ -177,7 +177,6 @@ public:
 	}
 };
 
-
 class Solution {
 
 
@@ -208,5 +207,50 @@ public:
 		dfs(0, k, 0, INT_MAX, nums1, nums2);
 
 		return ret;
+	}
+};
+
+class Solution {
+public:
+	long long maxScore(std::vector<int>& nums1, std::vector<int>& nums2, int k) {
+
+		int n = nums1.size();
+		std::vector<std::pair<int, int>> pairs(n);
+
+		// nums1[i]와 nums2[i]를 쌍으로 묶음
+		for (int i = 0; i < n; ++i) {
+			pairs[i] = { nums1[i], nums2[i] };
+		}
+
+		// nums2를 기준으로 내림차순 정렬
+		std::sort(pairs.begin(), pairs.end(), [](const std::pair<int, int>& a, const std::pair<int, int>& b) {
+			return b.second < a.second;
+			});
+
+		// 최소 힙을 사용하여 상위 k개의 nums1 값을 관리
+		std::priority_queue<int, std::vector<int>, std::greater<int>> topKHeap;
+		long long topKSum = 0;
+
+		// 처음 k개의 값을 topKSum에 더하고, 최소 힙에 넣음
+		for (int i = 0; i < k; ++i) {
+			topKSum += pairs[i].first;
+			topKHeap.push(pairs[i].first);
+		}
+
+		// 처음 k개의 쌍으로부터 계산한 초기 점수
+		long long answer = topKSum * pairs[k - 1].second;
+
+		// 나머지 요소들에 대해 반복하며 점수 계산
+		for (int i = k; i < n; ++i) {
+			// 최소 힙에서 가장 작은 값을 제거하고, 현재 값을 더함
+			topKSum += pairs[i].first - topKHeap.top();
+			topKHeap.pop();
+			topKHeap.push(pairs[i].first);
+
+			// 현재 점수 계산하고, 최대 점수 갱신
+			answer = std::max(answer, topKSum * pairs[i].second);
+		}
+
+		return answer;
 	}
 };
