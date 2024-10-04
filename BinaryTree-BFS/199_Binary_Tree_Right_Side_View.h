434ad924e3ce59d7c3729de6073701e5ce83ef10
ordinarydennis <ordinarydennis@gmail.com>
2024-10-04 13:26:51 +0900

    Binary Tree - BFS - 199. Binary Tree Right Side View(bfs, 4m) - 199. Binary Tree Right Side View(dfs, 3m)

--- BinaryTree-BFS/199_Binary_Tree_Right_Side_View.h
+++ BinaryTree-BFS/199_Binary_Tree_Right_Side_View.h
@@ -15,7 +15,7 @@ public:
 
 		if (!root)
 			return {};
-		
+
 		std::queue<TreeNode*> q;
 
 		q.push(root);
@@ -34,7 +34,7 @@ public:
 
 				if (i == 0)
 					ret.push_back(node->val);
-				
+
 				if (node->right)
 					q.push(node->right);
 				if (node->left)
@@ -79,7 +79,7 @@ public:
 		{
 			int size = q.size();
 
-			for(int i = 0; i < size; i++)
+			for (int i = 0; i < size; i++)
 			{
 				auto* node = q.front();
 
@@ -100,6 +100,149 @@ public:
 					ret.push_back(node->val);
 				}
 			}
+		}
+
+		return ret;
+	}
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+	vector<int> rightSideView(TreeNode* root) {
+
+		if (!root)
+		{
+			return {};
+		}
+
+		queue<TreeNode*> q;
+
+		q.push(root);
+
+		vector<int> ret;
+
+		while (!q.empty())
+		{
+			auto s = q.size();
+
+			int last = 0;
+
+			for (int i = 0; i < s; i++)
+			{
+				auto* n = q.front();
+
+				q.pop();
+
+				if (n->left)
+				{
+					q.push(n->left);
+				}
+
+				if (n->right)
+				{
+					q.push(n->right);
+				}
+
+				last = n->val;
+			}
+
+			ret.push_back(last);
+
+		}
+
+		return ret;
+	}
+};
+
+class Solution {
+
+	void dfs(int level, TreeNode* node, map<int, vector<int>>& m)
+	{
+		if (nullptr == node)
+		{
+			return;
+		}
+
+		m[level].push_back(node->val);
+
+		if (node->left)
+		{
+			dfs(level + 1, node->left, m);
+		}
+
+		if (node->right)
+		{
+			dfs(level + 1, node->right, m);
+		}
+	}
+
+
+public:
+	vector<int> rightSideView(TreeNode* root) {
+
+		vector<int> ret;
+
+		map<int, vector<int>> m;
+
+
+		dfs(0, root, m);
+
+		for (auto [_, list] : m)
+		{
+			ret.push_back(list[list.size() - 1]);
+		}
+
+		return ret;
+	}
+};
+
+
+class Solution {
+
+	void dfs(int level, TreeNode* node, map<int, int>& m)
+	{
+		if (nullptr == node)
+		{
+			return;
+		}
+
+		m[level] = node->val;
+
+		if (node->left)
+		{
+			dfs(level + 1, node->left, m);
+		}
+
+		if (node->right)
+		{
+			dfs(level + 1, node->right, m);
+		}
+	}
+
+
+public:
+	vector<int> rightSideView(TreeNode* root) {
+
+		vector<int> ret;
+
+		map<int, int> m;
+
+		dfs(0, root, m);
+
+		for (auto [_, last] : m)
+		{
+			ret.push_back(last);
 		}
 
 		return ret;
