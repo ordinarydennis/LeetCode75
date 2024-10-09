85325813852799181cba3a93902bdaea0ac95221
ordinarydennis <ordinarydennis@gmail.com>
2024-10-09 18:53:16 +0900

    DP - 55. Jump Game(6m, solved again, dfs, bfs) - 53. Maximum Subarray(solved again)
    Binary Tree - DFS
    - 872. Leaf-Similar Trees(5m)

--- BinaryTree-DFS/872_Leaf-Similar_Trees.h
+++ BinaryTree-DFS/872_Leaf-Similar_Trees.h
@@ -39,3 +39,50 @@ public:
 		return v1 == v2;
 	}
 };
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+
+	void dfs(TreeNode* node, vector<int>& v)
+	{
+		if(!node->left && !node->right)
+		{
+			v.push_back(node->val);
+			return;
+		}
+
+		if(node->left)
+		{
+			dfs(node->left, v);
+		}
+
+		if (node->right)
+		{
+			dfs(node->right, v);
+		}
+	}
+
+
+public:
+	bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+
+		vector<int> v1, v2;
+
+		dfs(root1, v1);
+		dfs(root2, v2);
+		
+		return v1 == v2;
+
+	}
+};
