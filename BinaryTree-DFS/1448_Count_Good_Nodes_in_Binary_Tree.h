6b92a3da6f2c15a51906edbae36dcaa9fcc4fb91
ordinarydennis <ordinarydennis@gmail.com>
2024-10-10 13:39:28 +0900

    Binary Tree - DFS - 1448. Count Good Nodes in Binary Tree(10m, dfs, bfs)

--- BinaryTree-DFS/1448_Count_Good_Nodes_in_Binary_Tree.h
+++ BinaryTree-DFS/1448_Count_Good_Nodes_in_Binary_Tree.h
@@ -79,3 +79,100 @@ public:
 
 	}
 };
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+
+	void dfs(TreeNode* node, int max, int& count)
+	{
+		if (max <= node->val)
+		{
+			max = node->val;
+			count++;
+		}
+	
+		if (node->left)
+			dfs(node->left, max, count);
+
+		if (node->right)
+			dfs(node->right, max, count);
+	}
+
+
+public:
+	int goodNodes(TreeNode* root) {
+
+		int count = 0;
+
+		dfs(root, INT_MIN, count);
+
+		return count;
+
+	}
+};
+
+
+
+class Solution {
+
+
+public:
+	int goodNodes(TreeNode* root) {
+
+
+		queue<pair<TreeNode* , TreeNode* >> q;
+
+		q.push( pair<TreeNode*, TreeNode* >{nullptr, root} );
+
+		int count = 0;
+
+		while (!q.empty())
+		{
+			int sz = q.size();
+
+			for (int i = 0; i < sz; i++)
+			{
+				auto p = q.front();
+
+				q.pop();
+
+				auto pNode = p.first;
+				auto cNode = p.second;
+
+				TreeNode* maxNode = pNode;
+
+				if (nullptr == maxNode || maxNode->val <= cNode->val)
+				{
+					count++;
+					maxNode = cNode;
+				}
+
+				if (cNode->left)
+				{
+					q.push(pair<TreeNode*, TreeNode* >{maxNode, cNode->left});
+				}
+
+				if (cNode->right)
+				{
+					q.push(pair<TreeNode*, TreeNode* >{maxNode, cNode->right});
+				}
+			}
+		}
+
+		return count;
+
+	}
+};
