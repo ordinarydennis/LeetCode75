597796f8c0d8594953c9dffbbb1478c12f775955
ordinarydennis <ordinarydennis@gmail.com>
2024-10-18 13:28:54 +0900

    Linked List - 2095. Delete the Middle Node of a Linked List(6m, solved another way)

--- LinkedList/2095_Delete_the_Middle_Node_of_a_Linked_List.h
+++ LinkedList/2095_Delete_the_Middle_Node_of_a_Linked_List.h
@@ -136,3 +136,41 @@ public:
 		return head;
 	}
 };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+	ListNode* deleteMiddle(ListNode* head) {
+
+		if (!head->next)
+			return nullptr;
+
+		auto* node = head;
+		int count = 0;
+
+		while (node)
+		{
+			count++;
+			node = node->next;
+		}
+
+		node = head;
+
+		for (int i = 0; i < count / 2 - 1; i++)
+			node = node->next;
+
+			node->next = node->next->next;
+		
+		return head;
+	}
+};
+
