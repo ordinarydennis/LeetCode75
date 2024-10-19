ee5404592a6c271bfe9b244f1c61ce284ed70421
ordinarydennis <ordinarydennis@gmail.com>
2024-10-19 20:47:37 +0900

    Linked List - 328. Odd Even Linked List(17m

--- LinkedList/328_Odd_Even_Linked_List.h
+++ LinkedList/328_Odd_Even_Linked_List.h
@@ -87,3 +87,51 @@ public:
 		return head;
 	}
 };
+
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+	ListNode* oddEvenList(ListNode* head) {
+
+		if(!head)
+			return nullptr;
+
+		auto oddHead = head;
+		auto evenHead = head->next;
+
+		auto odd = head;
+		auto even = head->next;
+
+		while (odd && even)
+		{
+			odd->next = even->next;
+
+			if (odd->next)
+			{
+				odd = odd->next;
+			}
+			else
+			{
+				break;
+			}
+			
+			even->next = odd->next;
+			even = even->next;
+		}
+
+		odd->next = evenHead;
+
+		return oddHead;
+	}
+};
