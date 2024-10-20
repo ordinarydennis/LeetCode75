e4c0aa2f2c588fef5737aed119bc2849e572ffee
ordinarydennis <ordinarydennis@gmail.com>
2024-10-20 22:33:14 +0900

    Linked List - 206. Reverse Linked List(5m, 9m)

--- LinkedList/206_Reverse_Linked_List.h
+++ LinkedList/206_Reverse_Linked_List.h
@@ -59,3 +59,93 @@ public:
 	}
 };
 
+
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+	ListNode* reverseList(ListNode* head) {
+
+		if (!head)
+		{
+			return nullptr;
+		}
+
+		auto* node = head;
+
+		vector<ListNode*> vec;
+
+		while(node)
+		{
+			vec.push_back(node);
+			node = node->next;
+		}
+
+		for (int i = vec.size() - 1; 1 <= i; i--)
+		{
+			vec[i]->next = vec[i - 1];
+		}
+
+		vec[0]->next = nullptr;
+
+
+		return vec[vec.size() - 1];
+
+
+	}
+};
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+	ListNode* reverseList(ListNode* head) {
+
+		if (!head)
+		{
+			return nullptr;
+		}
+
+		ListNode* pre = nullptr;
+		ListNode* cur = head;
+
+		while(cur)
+		{
+			auto* n = cur->next;
+
+			cur->next = pre;
+
+			pre = cur;
+
+			if (n)
+			{
+				cur = n;
+			}
+			else
+			{
+				break;
+			}
+		}
+
+		return cur;
+	}
+};
