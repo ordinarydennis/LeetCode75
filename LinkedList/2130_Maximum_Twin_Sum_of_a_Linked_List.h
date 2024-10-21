6ac4349740b0c5caa7d6565673df4ec013b62011
ordinarydennis <ordinarydennis@gmail.com>
2024-10-21 14:29:31 +0900

    Linked List - 2130. Maximum Twin Sum of a Linked List(12m) (solution using reversing list is fail)

--- LinkedList/2130_Maximum_Twin_Sum_of_a_Linked_List.h
+++ LinkedList/2130_Maximum_Twin_Sum_of_a_Linked_List.h
@@ -107,3 +107,77 @@ public:
 		return ret;
 	}
 };
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+	int pairSum(ListNode* head) {
+
+		vector<int> v;
+
+		auto* node = head;
+
+		while (node)
+		{
+			v.push_back(node->val);
+			node = node->next;
+		}
+
+		int max = v.size() - 1;
+		int ret = 0;
+
+		for (int i = 0; i <= max / 2; i++)
+		{
+			ret = std::max(ret, v[i] + v[max - i]);
+		}
+
+		return ret;
+	}
+};
+
+
+//fail
+class Solution {
+
+public:
+	int pairSum(ListNode* head) {
+
+		ListNode* pre = nullptr;
+		ListNode* cur = head;
+
+		int count = 0;
+
+		while (cur)
+		{
+			count++;
+			auto* next = cur->next;
+			cur->next = pre;
+			pre = cur;
+			cur = next;
+		}
+
+		auto* nodeLeft = head;
+		auto* nodeRight = pre; //reverseHead
+
+		int ret = 0;
+
+		while (nodeLeft && nodeRight)
+		{
+			ret = max(ret, nodeLeft->val + nodeRight->val);
+			nodeLeft = nodeLeft->next;
+			nodeRight = nodeRight->next;
+		}
+
+		return ret;
+	}
+};
