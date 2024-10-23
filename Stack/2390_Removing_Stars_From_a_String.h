970c615f4c390abee482fc5e0ecc514f221eea55
ordinarydennis <ordinarydennis@gmail.com>
2024-10-23 20:24:46 +0900

    Stack - 2390. Removing Stars From a String(4m)

--- Stack/2390_Removing_Stars_From_a_String.h
+++ Stack/2390_Removing_Stars_From_a_String.h
@@ -60,3 +60,96 @@ public:
 		return answer;
 	}
 };
+
+
+
+
+class Solution {
+public:
+    string removeStars(string s) {
+        
+		std::stack<char> st;
+
+		for(int i = 0; i < s.size(); i++)
+		{
+			if('*' == s[i] && !st.empty())
+			{
+				st.pop();
+			}
+			else
+			{
+				st.push(s[i]);
+			}
+		}
+
+		string ret;
+
+		while(!st.empty())
+		{
+			ret += st.top();
+			st.pop();
+		}
+
+		reverse(ret.begin(), ret.end());
+
+		return ret;
+    }
+};
+
+//memory limit
+class Solution {
+public:
+    string removeStars(string s) {
+        
+		std::stack<char> st;
+
+		for(int i = 0; i < s.size(); i++)
+		{
+			if('*' == s[i] && !st.empty())
+			{
+				st.pop();
+			}
+			else
+			{
+				st.push(s[i]);
+			}
+		}
+
+		string ret;
+
+		while(!st.empty())
+		{
+			ret = st.top() + ret;
+			st.pop();
+		}
+
+		return ret;
+    }
+};
+
+
+//two pointer
+class Solution {
+public:
+	string removeStars(string s) {
+
+		string ch;
+		int j = 0;
+
+		for (int i = 0; i < s.size(); i++) {
+			if (s[i] == '*') {
+				j--;
+			}
+			else {
+				ch[j++] = s[i];
+			}
+		}
+
+		string answer = "";
+		for (int i = 0; i < j; i++) {
+			answer.push_back(ch[i]);
+		}
+
+		return answer;
+	}
+};
