a68fff50110533b6112f3941922b268689250cb4
ordinarydennis <ordinarydennis@gmail.com>
2024-11-02 22:01:34 +0900

    Sliding Window - 643. Maximum Average Subarray I

--- SlidingWindow/643_Maximum_Average_Subarray_I.h
+++ SlidingWindow/643_Maximum_Average_Subarray_I.h
@@ -34,3 +34,21 @@ public:
 		return max;
 	}
 };
+
+
+class Solution {
+public:
+	double findMaxAverage(vector<int>& nums, int k) {
+
+		int sum = accumulate(nums.begin(), nums.begin() + k, 0);
+		int maxInt = sum;
+
+		for (int i = k; i < nums.size(); i++)
+		{
+			sum += nums[i] - nums[i - k];
+			maxInt = max(maxInt, sum);
+		}
+
+		return double(maxInt) / k;
+	}
+};
