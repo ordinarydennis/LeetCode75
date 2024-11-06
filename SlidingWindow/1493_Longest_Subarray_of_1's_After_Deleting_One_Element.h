ffc0b29b48aa0fda893ddc697902df29285c6ce4
ordinarydennis <ordinarydennis@gmail.com>
2024-11-06 19:43:29 +0900

    Sliding Window - 1493. Longest Subarray of 1's After Deleting One Element

--- SlidingWindow/1493_Longest_Subarray_of_1's_After_Deleting_One_Element.h
+++ SlidingWindow/1493_Longest_Subarray_of_1's_After_Deleting_One_Element.h
@@ -1,93 +1,128 @@
 class Solution {
 public:
-    int longestSubarray(vector<int>& nums) {
+	int longestSubarray(vector<int>& nums) {
 
-        int count = 0;
+		int count = 0;
 
-        int left = 0;
-        int right = 0;
+		int left = 0;
+		int right = 0;
 
-        for (; right < nums.size(); right++)
-        {
-            if (0 == nums[right])
-            {
-                count++;
-            }
+		for (; right < nums.size(); right++)
+		{
+			if (0 == nums[right])
+			{
+				count++;
+			}
 
-            if (1 < count)
-            {
-                if (0 == nums[left])
-                {
-                    count--;
-                }
-                left++;
-            }
-        }
+			if (1 < count)
+			{
+				if (0 == nums[left])
+				{
+					count--;
+				}
+				left++;
+			}
+		}
 
-        return right - left - 1;
-    }
+		return right - left - 1;
+	}
 };
 
 class Solution {
 public:
-    int longestSubarray(vector<int>& nums) {
+	int longestSubarray(vector<int>& nums) {
 
-        int zCount = 0;
+		int zCount = 0;
 
-        int left = 0; 
+		int left = 0; 
 
-        int ret = 0;
+		int ret = 0;
 
-        for (int i = 0; i < nums.size(); i++)
-        {
-            if (0 == nums[i])
-            {
-                zCount++;
-            }
+		for (int i = 0; i < nums.size(); i++)
+		{
+			if (0 == nums[i])
+			{
+				zCount++;
+			}
 
-            if (zCount <= 1)
-            {
-                ret = max(ret, i - left + 1);
-            }
+			if (zCount <= 1)
+			{
+				ret = max(ret, i - left + 1);
+			}
 
-            if (1 < zCount)
-            {
-                while(1 == nums[left])
-                {
-                    left++;
-                }
+			if (1 < zCount)
+			{
+				while(1 == nums[left])
+				{
+					left++;
+				}
 
-                left++;
-                zCount--;
-            }
-      
-        }
+				left++;
+				zCount--;
+			}
+	  
+		}
 
-        return ret - 1;
-    }
+		return ret - 1;
+	}
 };
 
 class Solution {
 public:
-    int longestSubarray(vector<int>& nums) {
+	int longestSubarray(vector<int>& nums) {
 
-        int zCount = 0;
-        int start = 0;
-        int ret = 0;
+		int zCount = 0;
+		int start = 0;
+		int ret = 0;
 
-        for (int i = 0; i < nums.size(); i++)
-        {
-            zCount += (0 == nums[i]) ? 1 : 0;
+		for (int i = 0; i < nums.size(); i++)
+		{
+			zCount += (0 == nums[i]) ? 1 : 0;
 
-            while (1 < zCount)
-            {
-                zCount -= (nums[start] == 0) ? 1 : 0;
-                start++;
-            }
+			while (1 < zCount)
+			{
+				zCount -= (nums[start] == 0) ? 1 : 0;
+				start++;
+			}
 
-            ret = max(ret, i - start);
-        }
+			ret = max(ret, i - start);
+		}
 
-        return ret;
-    }
+		return ret;
+	}
+};
+
+
+class Solution {
+public:
+	int longestSubarray(vector<int>& nums) {
+
+		int max = 0;
+		int remain = 1;
+		int left = 0;
+
+		for (int i = 0; i < nums.size(); i++)
+		{
+			if (0 == nums[i])
+			{
+				if (1 == remain)
+				{
+					remain--;
+				}
+				else
+				{
+					while (1 == nums[left])
+					{
+						left++;
+					}
+					left++;
+					remain++;
+				}
+			}
+
+			max = std::max(max, i - left);
+		}
+
+		return max - 1;
+	}
 };
