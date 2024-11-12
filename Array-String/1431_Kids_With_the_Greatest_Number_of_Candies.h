38fd6ba39759e3df4cb3adcdc02477e619f59139
ordinarydennis <ordinarydennis@gmail.com>
2024-11-12 13:41:17 +0900

    Array / String - 1431. Kids With the Greatest Number of Candies(4m)

--- Array-String/1431_Kids_With_the_Greatest_Number_of_Candies.h
+++ Array-String/1431_Kids_With_the_Greatest_Number_of_Candies.h
@@ -48,3 +48,18 @@ public:
 		return ret;
 	}
 };
+
+class Solution {
+public:
+	vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+
+		int max = *max_element(candies.begin(), candies.end());
+
+		vector<bool> ret(candies.size(), false);
+
+		for (int i = 0; i < candies.size(); i++)
+			ret[i] = max <= candies[i] + extraCandies;
+
+		return ret;
+	}
+};
