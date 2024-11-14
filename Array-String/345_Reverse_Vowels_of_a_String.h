8e7360bbe34c5c0765d2994fe15e5abdea1d42c2
ordinarydennis <ordinarydennis@gmail.com>
2024-11-15 01:18:04 +0900

    Array / String - 345. Reverse Vowels of a String(11m)

--- Array-String/345_Reverse_Vowels_of_a_String.h
+++ Array-String/345_Reverse_Vowels_of_a_String.h
@@ -1,24 +1,55 @@
 
 class Solution {
 public:
-    string reverseVowels(string s) {
+	string reverseVowels(string s) {
 
-        unordered_set<char> us = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+		unordered_set<char> us = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
 
-        int left = 0;
-        int right = s.size() - 1;
+		int left = 0;
+		int right = s.size() - 1;
 
-        while (left < right)
-        {
-            while (left < right && left < s.size() && 0 == us.count(s[left])) 
-                left++;
+		while (left < right)
+		{
+			while (left < right && left < s.size() && 0 == us.count(s[left])) 
+				left++;
 
-            while (left < right && 0 <= right && 0 == us.count(s[right])) 
-                right--;
+			while (left < right && 0 <= right && 0 == us.count(s[right])) 
+				right--;
 
-            swap(s[left++], s[right--]);
-        }
+			swap(s[left++], s[right--]);
+		}
 
-        return s;
-    }
+		return s;
+	}
+};
+
+
+class Solution {
+
+	bool isVowel(char c)
+	{
+		static unordered_set<char> v = { 'a', 'e','i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+
+		return v.count(c);
+	}
+
+public:
+	string reverseVowels(string s) {
+
+		int left = 0;
+		int right = s.size() - 1;
+
+		while (left < right)
+		{
+			while (left < right && false == isVowel(s[left])) left++;
+			
+			while (left < right  && false == isVowel(s[right])) right--;
+
+			swap(s[left], s[right]);
+			right--;
+			left++;
+		}
+
+		return s;
+	}
 };
